46a0635d22298010e2f3467df847d46fbd4ac405
Tim Webster <twebster17@gmail.com>
2016-10-16 14:53:26 -0700

    updated binning

--- src/sexChromosomeBamTraverse.py
+++ src/sexChromosomeBamTraverse.py
@@ -5,12 +5,20 @@ import sys
 from ggplot import *
 
 
-def traverseBam(samfile, chrom, start, stop, minDepth, afThresh, minAltReads):
+def traverseBam(samfile, chrom, start, stop, window_size, minDepth, afThresh, minAltReads):
+	
+	
+	depthCollector = []
+	readBalanceCollector = []
+	counter = 0
+	histDict = {}
+	
+	coord = []
 	depth = []
 	readBalance = []
-	counter = 0
-	passed = 0
+	windowCounter = 0
 	for pileupcolumn in samfile.pileup(chrom, start, stop):
+		
 		baseCountDict = {'A':0, 'C':0, 'G':0, 'T':0, 'N': 0}
 		for pileupread in pileupcolumn.pileups:
 			if not pileupread.is_del and not pileupread.is_refskip:
@@ -19,15 +27,26 @@ def traverseBam(samfile, chrom, start, stop, minDepth, afThresh, minAltReads):
 		numRef = baseCounts[-1]
 		numAlt = baseCounts[-2]
 		totalDepth = numRef + numAlt
+		
 		if totalDepth >= minDepth:
-			coord = int(pileupcolumn.pos)
 			depth.append(totalDepth)
 			alleleFraction = float(numAlt) / totalDepth
 			if numAlt >= minAltReads and alleleFraction >= afThresh:
 				readBalance.append(alleleFraction)
-				passed += 1
-			else:
-				readBalance.append(-1)
+				
+		windowCounter += 1
+		if windowCounter == window_size:
+			### process window
+			depthCollector.append(np.mean(np.asarray(depth)))
+			readBalanceCollector.append(np.mean(np.asarray(readBalance)))
+			
+			### Reset counters
+			windowCounter == 0
+			depth = []
+			readBalance = []
+			coord.append(int(pileupcolumn.pos))
+		
+		
 		counter += 1
 		if counter % 100000 == 0:
 			print "%d sites processed, %d of which passed filters" % (counter, passed)
@@ -53,6 +72,8 @@ def main():
 						help="DEFAULT is chrX.  The name of the X chromosome scaffold.")
 	parser.add_argument("--chrY_name", default="chrY",
 						help="DEFAULT is chrY.  The name of the Y chromosome scaffold.")
+	parser.add_argument("--window_size", type=int, default=50000,
+						help="DEFAULT is 50000.  Window size for sliding window calculations. Integer.")
 	parser.add_argument("--minimum_depth", type=int, default=2,
 						help="DEFAULT is 2. Minimum depth for a site to be considered. Integer.")
 	parser.add_argument("--minimum_alt_allele_reads", type=int, default=1,
