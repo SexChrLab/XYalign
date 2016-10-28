88580abffd3c09520635069f56abc883cc41e048
tnphung <tnphung@ucla.edu>
2016-10-28 16:53:56 -0700

    Add an argument when calling python script

--- Files/lastZ/generate_reference_genome_masks_pipeline.sh
+++ Files/lastZ/generate_reference_genome_masks_pipeline.sh
@@ -63,48 +63,47 @@ arg_1=$analysis_dir/$chr"_"$xKb_windowSize"_nonOverlapping.txt"
 arg_2=$analysis_dir/$chr"_target_clean_joined.txt"
 arg_3=$analysis_dir/$chr"_query_clean_joined.txt"
 arg_4=50000
-arg_5=$analysis_dir/"target_out_bufferRegion"$arg_4".txt"
-arg_6=$analysis_dir/"query_out_bufferRegion"$arg_4".txt"
-arg_7=$analysis_dir/"window_query_out_bufferRegion"$arg_4".txt"
-arg_8=$analysis_dir/"toMaskRegions.bed" ### masked regions in bed format 
-arg_9=$analysis_dir/"identityMapped.bed" ### regions that are on the identity line in bed format 
-python $scripts_dir/generate_masks.py $arg_1 $arg_2 $arg_3 $arg_4 $arg_5 $arg_6 $arg_7 $arg_8 $arg_9
-#python $scripts_dir/generate_masks.py $arg_1 $arg_2 $arg_3 $arg_4 $arg_5 $arg_6 $arg_7
+arg_5=$analysis_dir/$chr"_target_out_bufferRegion"$arg_4".txt"
+arg_6=$analysis_dir/$chr"_query_out_bufferRegion"$arg_4".txt"
+arg_7=$analysis_dir/$chr"_window_query_out_bufferRegion"$arg_4".txt"
+arg_8=$analysis_dir/$chr"_toMaskRegions.bed" ### masked regions in bed format 
+arg_9=$analysis_dir/$chr"_identityMapped.bed" ### regions that are on the identity line in bed format 
+python $scripts_dir/generate_masks.py $arg_1 $arg_2 $arg_3 $arg_4 $arg_5 $arg_6 $arg_7 $arg_8 $arg_9 $chr
+
 
 ###############################################################################################
 # STEP 07: Plotting for visualization
 ###############################################################################################
 # A. Generate the rdotplot format for the "masked" regions
-paste $arg_5 $arg_6 > $analysis_dir/"target_query_out_bufferRegion"$arg_4".rdotplot"
-Rscript $scripts_dir/dotplot.R $analysis_dir/"target_query_out_bufferRegion"$arg_4".rdotplot" $analysis_dir/$chr"_"$chr"_masked_regions.jpeg"
+paste $arg_5 $arg_6 > $analysis_dir/$chr"_target_query_out_bufferRegion"$arg_4".rdotplot"
+Rscript $scripts_dir/dotplot.R $analysis_dir/$chr"_target_query_out_bufferRegion"$arg_4".rdotplot" $analysis_dir/$chr"_"$chr"_masked_regions.jpeg"
 
 # B. Overlaying
-Rscript $scripts_dir/dotplot_overlap.R $analysis_dir/$chr"_"$chr".rdotplot" $analysis_dir/"target_query_out_bufferRegion"$arg_4".rdotplot" $analysis_dir/$chr"_"$chr"overlay_with_maskedRegions.jpeg"
+Rscript $scripts_dir/dotplot_overlap.R $analysis_dir/$chr"_"$chr".rdotplot" $analysis_dir/$chr"_target_query_out_bufferRegion"$arg_4".rdotplot" $analysis_dir/$chr"_"$chr"overlay_with_maskedRegions.jpeg"
 
 ##############################################################################################
 # STEP 08: Compute some statistics, ie. how many base pairs are uniquely mapped vs multimapped
 ##############################################################################################
 
 # A. Sort the 2 bed files
-sort -n -k 2,2 $analysis_dir/"toMaskRegions.bed" > $analysis_dir/"toMaskRegions_sorted.bed"
-sort -n -k 2,2 $analysis_dir/"identityMapped.bed" > $analysis_dir/"identityMapped_sorted.bed"
+sort -n -k 2,2 $analysis_dir/$chr"_toMaskRegions.bed" > $analysis_dir/$chr"_toMaskRegions_sorted.bed"
+sort -n -k 2,2 $analysis_dir/$chr"_identityMapped.bed" > $analysis_dir/$chr"_identityMapped_sorted.bed"
 
 # B. Use bedtools merge
 
-bedtools merge -i $analysis_dir/"toMaskRegions_sorted.bed" > $analysis_dir/"toMaskRegions_sorted_merged.bed"
-bedtools merge -i $analysis_dir/"identityMapped_sorted.bed" > $analysis_dir/"identityMapped_sorted_merged.bed"
+bedtools merge -i $analysis_dir/$chr"_toMaskRegions_sorted.bed" > $analysis_dir/$chr"_toMaskRegions_sorted_merged.bed"
+bedtools merge -i $analysis_dir/$chr"_identityMapped_sorted.bed" > $analysis_dir/$chr"_identityMapped_sorted_merged.bed"
 
 # C. Compute the number of base pair within each region
-awk '{ $4 = $3 - $2} 1' $analysis_dir/"toMaskRegions_sorted_merged.bed" > $analysis_dir/"toMaskRegions_sorted_merged_diff.bed"
+awk '{ $4 = $3 - $2} 1' $analysis_dir/$chr"_toMaskRegions_sorted_merged.bed" > $analysis_dir/$chr"_toMaskRegions_sorted_merged_diff.bed"
 
-awk '{ $4 = $3 - $2} 1' $analysis_dir/"identityMapped_sorted_merged.bed" > $analysis_dir/"identityMapped_sorted_merged_diff.bed"
+awk '{ $4 = $3 - $2} 1' $analysis_dir/$chr"_identityMapped_sorted_merged.bed" > $analysis_dir/$chr"_identityMapped_sorted_merged_diff.bed"
 
-toMaskSum=`awk '{ sum += $4 } END {print sum} ' $analysis_dir/"toMaskRegions_sorted_merged_diff.bed"`
-identityMappedSum=`awk '{ sum += $4 } END {print sum} ' $analysis_dir/"identityMapped_sorted_merged_diff.bed"`
+toMaskSum=`awk '{ sum += $4 } END {print sum} ' $analysis_dir/$chr"_toMaskRegions_sorted_merged_diff.bed"`
+identityMappedSum=`awk '{ sum += $4 } END {print sum} ' $analysis_dir/$chr"_identityMapped_sorted_merged_diff.bed"`
 
 echo 'Total number of base pairs that are masked is' $toMaskSum
 echo 'Total number of base pairs that are on the identity line is' $identityMappedSum
-
 
 
 
