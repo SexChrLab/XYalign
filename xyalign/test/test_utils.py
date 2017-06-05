0c0979522f7538348e1b2fbedb049b8624fe7b7c
thw17 <twebster17@gmail.com>
2017-06-04 18:36:00 -0700

    added some utils testing

--- xyalign/test/test_utils.py
+++ xyalign/test/test_utils.py
@@ -1,11 +1,26 @@
 import os
 import pytest
+from xyalign import bam
+from xyalign import reftools
 from xyalign import utils
+
+# Get current directory
+dir = os.path.dirname(__file__)
+
+
+def read_bed(file):
+	with open(file, "r") as f:
+		return_list = []
+		for line in f:
+			return_list.append(line.strip().split())
+	return return_list
 
 
 def teardown_module(function):
-	if os.path.exists(os.path.join(".", "temp_test")):
-		os.rmdir(os.path.join(".", "temp_test"))
+	if os.path.exists(os.path.join(dir, "merged.bed")):
+		os.remove(os.path.join(dir, "merged.bed"))
+	if os.path.exists(os.path.join(dir, "temp_test")):
+		os.rmdir(os.path.join(dir, "temp_test"))
 
 
 def test_validate_external_prog():
@@ -16,10 +31,29 @@ def test_validate_external_prog():
 
 
 def test_validate_dir_exist():
-	assert utils.validate_dir(".", "temp_test") is False
-	assert utils.validate_dir(".", "temp_test") is True
+	assert utils.validate_dir(dir, "temp_test") is False
+	assert utils.validate_dir(dir, "temp_test") is True
 
 
-# def test_chromosome_bed():
-# 	with pytest.raises(RuntimeError):
-# 		utils.chromosome_bed("foo", "foo.txt", ["foo"])
+def test_check_bam_fasta_compatibility():
+	test_bam = bam.BamFile(
+		os.path.join(dir, "toy.bam"), "samtools", no_initial_index=True)
+	test2_bam = bam.BamFile(
+		os.path.join(dir, "tinyheader.bam"), "samtools", no_initial_index=True)
+	test_fasta = reftools.RefFasta(
+		os.path.join(dir, "toy.fasta"), "samtools", "bwa", no_initial_index=True)
+	test2_fasta = reftools.RefFasta(
+		os.path.join(dir, "toy3.fasta"), "samtools", "bwa", no_initial_index=True)
+	assert utils.check_bam_fasta_compatibility(test_bam, test_fasta) is True
+	assert utils.check_bam_fasta_compatibility(test2_bam, test_fasta) is False
+	assert utils.check_bam_fasta_compatibility(test_bam, test2_fasta) is False
+
+
+def test_merge_bed_files():
+	path = utils.merge_bed_files(
+		os.path.join(dir, "merged.bed"),
+		os.path.join(dir, "toy_mask.bed"),
+		os.path.join(dir, "toy_mask2.bed"))
+	assert path == os.path.join(dir, "merged.bed")
+	assert os.path.exists(os.path.join(dir, "merged.bed"))
+	assert read_bed(os.path.join(dir, "merged.bed")) == [["seq1", "0", "25"]]
